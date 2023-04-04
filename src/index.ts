3971cd2f597392a6961694002c9f3201d0f3b994
Wilson Foo Yu Kang <wilson.foo@customautosys.com>
2023-04-04 23:33:23 +0800

    Put in some lines.

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@ import type{
 	AxiosPromise
 }from 'axios';
 
-export default function axiosCordovaAdapter(
+export default async function axiosCordovaAdapter(
 	config:InternalAxiosRequestConfig
 ):AxiosPromise{
-	return null;
+	cordova.plugin.http.setRequestTimeout(config.timeout||0);
+	if(config.data instanceof URLSearchParams){
+		cordova.plugin.http.setDataSerializer('urlencoded');
+	}else if(config.data&&typeof config.data==='object'){
+		cordova.plugin.http.setDataSerializer('json');
+	}
 };
